f1b6571478031c8b2ab07e07cbc998a698140425
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-21 00:19:02 +0500

    done in chap15

---
+++ script.js
@@ -0,0 +1,43 @@
+// chap 15  Array
+
+
+// create in array 
+// const fruit=[];
+// console.log(fruit);
+
+// const fruits=["🍊","🍓","🍐"];
+// console.log(fruits);
+
+// const anamils=["donky","hourse","spider"];
+// console.log(anamils);
+
+// const fruits=["mangoo","bananan","peach"];
+// console.log(fruits);
+
+
+// const hotelRoom=["ali","muhammad","zahid","waqas","halid"];
+// console.log(hotelRoom[1]);
+// console.log(hotelRoom[0]);
+// console.log(hotelRoom[1]);
+// console.log(hotelRoom[2]);
+// console.log(hotelRoom[3]);
+// console.log(hotelRoom[4]);
+
+// const food=["lobia","sag","alogan","gopi","badrang","checken"];
+// console.log(food[5]);
+// console.log(food[3]);
+// console.log(food[4]);
+
+// const anamils=["cow","bupilo","marhoor","hourse"];
+// // console.log(anamils[1]);
+// console.log(anamils[3]);
+// console.log(anamils[2]);
+
+// mix type of data include in array 
+
+// const mixData=["1","hello","true","3.5"];
+// console.log(mixData[1]);
+// console.log(mixData[2]);
+// console.log(mixData[3]);
+
+
